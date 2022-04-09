01edb652ded83620f764b17dd7b884eb9b2271a4
Alex Ross <alxrg@me.com>
2022-04-08 20:05:38 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card } from 'react-bootstrap';
 
 function App() {
+
+  const [name, setName] = useState("")
+  const [peperoni, setPeperoni] = useState(false)
+  const [sausage, setSausage] = useState(false)
+  const [peppers, setPeppers] = useState(false)
+  const [onions, setOnions] = useState(false)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        
+        <Card>
+          <h1>
+          Pizza Order
+        </h1>
+        <form>
+          <label>
+            Name:
+            <input 
+              value={name} 
+              onChange={(e) => setName(e.target.value)}>
+            </input>
+          </label>
+          <br></br>
+          <label>
+            <input
+              type="checkbox"
+              checked={peperoni}
+              onChange={() => setPeperoni(!peperoni)}
+            /> 
+            Peperoni
+          </label>
+          <br></br>
+          <label>
+            <input
+              type="checkbox"
+              checked={sausage}
+              onChange={() => setSausage(!sausage)}
+            />
+            Sausage 
+          </label>
+          <br></br>
+          <label>
+            <input
+              type="checkbox"
+              checked={peppers}
+              onChange={() => setPeppers(!peppers)}
+            /> 
+            Peppers
+          </label>
+          <br></br>
+          <label>
+            <input
+              type="checkbox"
+              checked={onions}
+              onChange={() => setOnions(!onions)}
+            /> 
+            Onions
+          </label>
+        </form>
+        </Card>
+
+      <Card>
+        <h3>Order</h3>
+        <h3>{name}</h3>
+        {peperoni && <p>Peperoni</p>}
+        {sausage && <p>Sausage</p>}
+        {peppers && <p>Peppers</p>}
+        {onions && <p>Onions</p>}
+      </Card>
     </div>
   );
 }
